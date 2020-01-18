c2115eb0ebaf4df1fbf203b9c0d5342792a217a0
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-01-17 16:33:43 -0800

    add rough changes to js.scripts Not quite fully functional.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Back-end //
+
+var result1 = $("value").val(); 
+
+var result2 = $("value#").val();
+
+var result3 = $("value").val();
+
+var result4 = $("value").val();
+
+var result5 = $("value").val();
+
+// Front-end //
+
+$(document).ready(function() {
+  $("form#food").submit(function(event) {
+    event.preventDefault();
+    if (result1 === result2); {
+      $("output").text("Congrats! You are front-end specialized programmer! Stick with html and css.")
+    else if (result1 != result2 ) {
+      $("output").text("Maybe you should reconsider your field of choice...");
+    });
+  });
+});
