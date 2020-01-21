13e0f8dbc9aa1f81cc35cd042229b90e2cf76a3c
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-01-21 15:22:10 -0800

    add changes to form

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 // Back-end //
 
-var result1 = $("value").val(); 
+// var result1 = $("value").val(); 
 
-var result2 = $("value#").val();
+// var result2 = $("value#").val();
 
-var result3 = $("value").val();
+// var result3 = $("value").val();
 
-var result4 = $("value").val();
+// var result4 = $("value").val();
 
-var result5 = $("value").val();
+// var result5 = $("value").val();
 
-// Front-end //
+// // Front-end //
 
-$(document).ready(function() {
-  $("form#food").submit(function(event) {
-    event.preventDefault();
-    if (result1 === result2); {
-      $("output").text("Congrats! You are front-end specialized programmer! Stick with html and css.")
-    else if (result1 != result2 ) {
-      $("output").text("Maybe you should reconsider your field of choice...");
-    });
-  });
-});
+// $(document).ready(function() {
+//   $("form#food").submit(function(event) {
+//     event.preventDefault();
+//     if (result1 === result2); {
+//       $("output").text("Congrats! You are front-end specialized programmer! Stick with html and css.")
+//     else if (result1 != result2 ) {
+//       $("output").text("Maybe you should reconsider your field of choice...");
+//     });
+//   });
+// });
 
 
 // Messy code, I need as much help as possible to run conditionals within the function to return a string as the result of the users choices //
+
