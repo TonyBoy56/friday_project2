871ad76fabdc07d2f053e122ae0470b2db684cd8
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-01-23 11:55:06 -0800

    add start of constructive JS changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 $(document).ready(function() {
-  $("form#food").submit(function (event) {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
-    var result1 = $("food#question1").val();
+    var result1 = $("input[name=question1]").val();
 
-    var result2 = $("activity#question2").val();
+    var result2 = $("input[name=question2").val();
 
-    var result3 = $("language#question3").val();
+    var result3 = $("input[name=question3]").val();
 
-    var result4 = $("coffee#question4").val();
+    var result4 = $("input[name=question4]").val();
 
-    var result5 = $("color#question5").val();
-
+    var result5 = $("input[name=question5]").val();
+  
     if (result1 === result2); {
-      $("#output").text("Congrats! You are a front-end programmer! Stick with html and css.");
-    {
-
-  } else if (result1 != result2) {
-      $("#output").text("Maybe you should reconsider your field of choice...");
-        }
-      }
+      $("#output").text("")
     }
   });
 });
