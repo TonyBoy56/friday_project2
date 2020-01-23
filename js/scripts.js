04a62d1736101aa826a36a470c7c3b73fe810360
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-01-22 17:52:11 -0800

    add work-in-progress fix for scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,30 @@
-// Back-end //
+$(document).ready(function() {
+  $("form#food").submit(function (event) {
+    event.preventDefault();
+    var result1 = $("food#question1").val();
 
-// var result1 = $("value").val(); 
+    var result2 = $("activity#question2").val();
 
-// var result2 = $("value#").val();
+    var result3 = $("language#question3").val();
 
-// var result3 = $("value").val();
+    var result4 = $("coffee#question4").val();
 
-// var result4 = $("value").val();
+    var result5 = $("color#question5").val();
 
-// var result5 = $("value").val();
+    if (result1 === result2); {
+      $("#output").text("Congrats! You are a front-end programmer! Stick with html and css.");
+    {
 
-// // Front-end //
-
-// $(document).ready(function() {
-//   $("form#food").submit(function(event) {
-//     event.preventDefault();
-//     if (result1 === result2); {
-//       $("output").text("Congrats! You are front-end specialized programmer! Stick with html and css.")
-//     else if (result1 != result2 ) {
-//       $("output").text("Maybe you should reconsider your field of choice...");
-//     });
-//   });
-// });
-
-
-// Messy code, I need as much help as possible to run conditionals within the function to return a string as the result of the users choices //
-
-// New Back-end logic //
-
-// New Front-end logic //
-
-$(document).ready(function () {
-    $("#formOne").submit(function (event) {
-        
-        event.preventDefault();
-
-    });
+  } else if (result1 != result2) {
+      $("#output").text("Maybe you should reconsider your field of choice...");
+        }
+      }
+    }
+  });
 });
+
+// if (result1 === result2); {
+//   $("output").text("Congrats! You are front-end specialized programmer! Stick with html and css.")
+//   else if (result1 != result2) {
+//     $("output").text("Maybe you should reconsider your field of choice...");
+//   });
