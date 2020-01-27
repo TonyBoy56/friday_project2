720ae5e6a5ad2719bd3c0f444de0b89a0a206512
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-01-27 12:59:08 -0800

    add syntax changes that makes the code cleaner and run efficiently.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var result1 = $("input[name=question1]").val();
+    var result1 = $("input[name=question1]:checked").val();
 
-    var result2 = $("input[name=question2").val();
+    var result2 = $("input[name=question2]:checked").val();
 
-    var result3 = $("input[name=question3]").val();
+    var result3 = $("input[name=question3]:checked").val();
 
-    var result4 = $("input[name=question4]").val();
+    var result4 = $("input[name=question4]:checked").val();
 
-    var result5 = $("input[name=question5]").val();
+    var result5 = $("input[name=question5]:checked").val();
   
     if (result1 === result2) {
       $("#output").text("Nice choice!Try some HTML/CSS!");
     } else if (result2 === result3) {
       $("#output").text("Hmmm... It seems back-end programming may be suitable for you."); 
     } else if (result3 === result4) {
-      ("#output").text("Nice! plenty of caffiene is a great choice for some front-end focused development!" );
+      $("#output").text("Nice! plenty of caffiene is a great choice for some front-end focused development!" );
     } else if (result4 === result5) {
-      ("#output").text("You know, maybe it's worth considering taking up something else.");
+      $("#output").text("You know, maybe it's worth considering taking up something else.");
     }
   });
 });
